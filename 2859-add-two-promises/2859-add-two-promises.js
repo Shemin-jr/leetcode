0296fed805e5b27313af25de7fbd3ddb5d80fad2
Shemin-jr <sheminmuhammed594@gmail.com>
2025-11-13 20:25:19 +0530

    Time: 58 ms (46.25%), Space: 54 MB (89.22%) - LeetHub

---
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+ const [p1 , p2]= await Promise.all([promise1, promise2]);
+ return p1+p2;
+
+    
+};
+
